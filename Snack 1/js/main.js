874cea73c97a06d3694b0a8914ffde5618821530
Paolo Giovannini <giovannini13@gmail.com>
2023-03-10 14:34:34 +0100

    snack 1 completato

---
+++ Snack 1/js/main.js
@@ -0,0 +1,34 @@
+const biciDom = document.getElementById('bici')
+
+const biciclette = [
+    {
+        nome: "bici rossa",
+        peso: 5
+    },
+    {
+        nome: "bici bianca",
+        peso: 4
+    },
+    {
+        nome: "bici gialla",
+        peso: 7
+    },
+    {
+        nome: "bici viola",
+        peso: 3
+    }
+];
+  
+  let biciLeggera = biciclette[0];
+  
+  biciclette.forEach(elementoCorrente => {
+    console.log(elementoCorrente)
+    const {peso} = elementoCorrente;
+    console.log(peso)
+    if(peso <= biciLeggera.peso) {
+      biciLeggera = elementoCorrente;
+    }
+  });
+  
+  biciDom.innerHTML = `La bici più leggera è la ${biciLeggera.nome} con un peso di ${biciLeggera.peso} kg.`;
+
